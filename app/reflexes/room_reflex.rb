1a6d6670d6237d16ab59340620e268e6082d66f6
Parker Scobey <parkerscobey@Parkers-MacBook-Air.local>
2021-01-19 16:49:37 -0600

    copied controller into javascripts directory

--- app/reflexes/room_reflex.rb
+++ app/reflexes/room_reflex.rb
@@ -1,11 +1,17 @@
 class RoomReflex < ApplicationReflex
 
     def add_user
-        Room.find(element.dataset[:room_id]).users << User.find(element.dataset[:user_id].to_i)
+        u = User.find(element.dataset[:user_id].to_i)
+        Room.find(element.dataset[:room_id]).users << u
+        message = "#{u.first_name} #{u.last_name} added"
+        morph "#user-#{u.id}", "<div class='text-center'><div class='text-success px-1 py-0'>#{message}</div></div>"
     end
 
     def remove_user
+        u = User.find(element.dataset[:user_id].to_i)
         Room.find(element.dataset[:room_id]).users.delete(element.dataset[:user_id].to_i)
+        message = "#{u.first_name} #{u.last_name} removed"
+        morph "#user-#{u.id}", "<div class='text-center'><div class='text-danger px-1 py-0'>#{message}</div></div>"
     end
 
 end
